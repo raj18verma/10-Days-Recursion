23d4bdd15e3164f69817d550c2c2b7ea4af7565d
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-07-25 18:09:56 +0530

    Day 4 question 2

---
+++ Letter Combination of a number.cpp
@@ -0,0 +1,33 @@
+
+    void solve(string s, string output, int index, vector<string>& ans, string mapping[] ) {
+        
+        //base case
+        if(index >= s.length()) {
+            ans.push_back(output);
+            return;
+        }
+        
+        int number = s[index] - '0';
+        string value = mapping[number];
+        
+        for(int i=0; i<value.length(); i++) {
+            output.push_back(value[i]);
+            solve(s, output, index+1, ans, mapping);
+            output.pop_back();
+        }
+        
+    }
+
+vector<string> combinations(string s){
+	
+	// Write your code here
+	    vector<string> ans;
+        if(s.length()==0)
+            return ans;
+        string output;
+        int index = 0;
+        string mapping[10] = {"", "", "abc", "def", "ghi", "jkl","mno","pqrs","tuv","wxyz"};
+        solve(s, output, index, ans, mapping);
+        return ans;
+	
+}
